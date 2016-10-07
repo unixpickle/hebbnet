a8ba0ca03ba57057398e97e85ffb379b48ebafc2
Alex Nichol <unixpickle@gmail.com>
2016-10-07 15:45:23 -0400

    print MNIST correctness histogram
    Now, the output from the MNIST rater looks something like this:
    
    Got 541/670 (80.75%) 0:0.94 1:0.98 2:0.84 3:0.75 4:0.77 5:0.38 6:0.84 7:0.84 8:0.77 9:0.88
    
    It lets you analyze which digits the network can recognize better than others. This might help analyze how well the network is actually using long-term information (versus just looking at the "end" of the image)

--- experiments/mnist/rater/main.go
+++ experiments/mnist/rater/main.go
@@ -16,6 +16,11 @@ import (
 	"github.com/unixpickle/weakai/rnn/seqtoseq"
 )
 
+type result struct {
+	label int
+	right bool
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "model_file")
@@ -43,7 +48,7 @@ func main() {
 		}
 		close(ch)
 	}()
-	outChan := make(chan bool)
+	outChan := make(chan result)
 
 	var wg sync.WaitGroup
 	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
@@ -60,32 +65,44 @@ func main() {
 	}()
 
 	var total, correct int
-	for right := range outChan {
+	correctMap := map[int]int{}
+	totalMap := map[int]int{}
+	for res := range outChan {
 		total++
-		if right {
+		totalMap[res.label]++
+		if res.right {
 			correct++
+			correctMap[res.label]++
 		}
-		fmt.Printf("\rGot %d/%d (%.02f%%)   ", correct, total,
-			100*float64(correct)/float64(total))
+		histogram := ""
+		for i := 0; i < 10; i++ {
+			if i != 0 {
+				histogram += " "
+			}
+			ratio := float64(correctMap[i]) / float64(totalMap[i])
+			if totalMap[i] == 0 {
+				ratio = 0
+			}
+			histogram += fmt.Sprintf("%d:%0.2f", i, ratio)
+		}
+		fmt.Printf("Got %d/%d (%.02f%%) %s\n", correct, total,
+			100*float64(correct)/float64(total), histogram)
 	}
 }
 
-func rateSamples(b rnn.Block, in <-chan seqtoseq.Sample, out chan<- bool) {
+func rateSamples(b rnn.Block, in <-chan seqtoseq.Sample, out chan<- result) {
 	for input := range in {
 		runner := rnn.Runner{Block: b}
 		for _, vec := range input.Inputs[:len(input.Inputs)-1] {
 			runner.StepTime(vec)
 		}
 		outVec := runner.StepTime(input.Inputs[len(input.Inputs)-1])
-		maxVal := math.Inf(-1)
-		maxIdx := 0
-		for i, x := range outVec {
-			if x > maxVal {
-				maxVal = x
-				maxIdx = i
-			}
+		label := maxIdx(input.Outputs[len(input.Outputs)-1])
+		output := maxIdx(outVec)
+		out <- result{
+			label: label,
+			right: label == output,
 		}
-		out <- (input.Outputs[len(input.Outputs)-1][maxIdx] == 1)
 	}
 }
 
@@ -100,4 +117,16 @@ func recurrentSample(sample mnist.Sample) seqtoseq.Sample {
 	outVec[sample.Label] = 1
 	resSample.Outputs = append(resSample.Outputs, outVec)
 	return resSample
+}
+
+func maxIdx(vec linalg.Vector) int {
+	maxVal := math.Inf(-1)
+	maxIdx := 0
+	for i, x := range vec {
+		if x > maxVal {
+			maxVal = x
+			maxIdx = i
+		}
+	}
+	return maxIdx
 }
